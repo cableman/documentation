de2baceab3805eb1dfa8aa094bc6be024a795ee8
Brian Jensen <mail@magnify.dk>
2013-11-19 14:51:11 +0100

    Add basic framework files

---
+++ mockups/index.php
@@ -0,0 +1,23 @@
+<?php
+  @include $_SERVER['DOCUMENT_ROOT']. '/inc/head.inc';
+?>
+<body id="top">
+<header class="header" role="banner">
+  <div class="header--inner">
+    <a href="#top" class="logo--link"><img src="logo.png" alt="" class="logo--image"></a>
+    <nav class="navigation">
+      <a href="#galleri" title="Galleri & inspiration">Galleri & inspiration</a>
+      <a href="#priser" title="Priser">Priser</a>
+      <a href="#info" title="Info">Information</a>
+      <a href="#kontakt" title="Kontakt">Kontakt</a>
+    </nav>
+  </div>
+</header>
+<div class="primary-content">
+
+</div>
+<div class="secondary-content">
+
+</div>
+</body>
+</html>
